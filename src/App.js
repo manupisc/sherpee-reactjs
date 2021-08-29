d745269a784aa8db26b1c17582c9e03c7ada3889
Manu <manu@sherpee.com>
2021-08-29 22:26:09 +0200

    add initial page and todo style

--- src/App.js
+++ src/App.js
@@ -2,23 +2,97 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = [];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          // onKeyPress={(e) => {
+          //   if (e.key === "Enter") {
+          //     send({ type: "NEWTODO.COMMIT", value: e.target.value });
+          //   }
+          // }}
+          // onChange={(e) =>
+          //   send({ type: "NEWTODO.CHANGE", value: e.target.value })
+          // }
+          onChange={console.log}
+          value={'todo'}
+        />
       </header>
-    </div>
+      <section className="main">
+        <input
+          id="toggle-all"
+          className="toggle-all"
+          type="checkbox"
+          checked={false}
+          onChange={console.log}
+          // onChange={() => {
+          //   send(markEvent);
+          // }}
+        />
+        <label htmlFor="toggle-all" title={`Mark all as {mark}`}>
+          Mark all as {/*mark*/}
+        </label>
+        <ul className="todo-list">
+          {/* {filteredTodos.map((todo) => (
+            <Todo key={todo.id} todoRef={todo.ref} />
+          ))} */}
+        </ul>
+      </section>
+      {!!todos.length && (
+        <footer className="footer">
+          <span className="todo-count">
+            {/* <strong>{numActiveTodos}</strong> item */}
+            {/* {numActiveTodos === 1 ? "" : "s"} left */}
+          </span>
+          <ul className="filters">
+            <li>
+              <a
+                // className={cn({
+                //   selected: filter === "all"
+                // })}
+                href="#/"
+              >
+                All
+              </a>
+            </li>
+            <li>
+              <a
+                // className={cn({
+                //   selected: filter === "active"
+                // })}
+                href="#/active"
+              >
+                Active
+              </a>
+            </li>
+            <li>
+              <a
+                // className={cn({
+                //   selected: filter === "completed"
+                // })}
+                href="#/completed"
+              >
+                Completed
+              </a>
+            </li>
+          </ul>
+          {/* {numActiveTodos < todos.length && (
+            <button
+              onClick={(_) => send("CLEAR_COMPLETED")}
+              className="clear-completed"
+            >
+              Clear completed
+            </button>
+          )} */}
+        </footer>
+      )}
+    </section>
   );
 }
 
